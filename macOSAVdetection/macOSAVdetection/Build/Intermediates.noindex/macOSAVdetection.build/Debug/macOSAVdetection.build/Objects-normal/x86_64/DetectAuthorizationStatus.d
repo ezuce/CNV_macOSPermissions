c39550ea245f94ca81044b3a1ae0b71db5e07be2
Build Linux <seevoghbuild@ezuce.com>
2020-11-02 17:10:56 +0000

    Add AV Permission

---
+++ macOSAVdetection/macOSAVdetection/Build/Intermediates.noindex/macOSAVdetection.build/Debug/macOSAVdetection.build/Objects-normal/x86_64/DetectAuthorizationStatus.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/seevogh/work/macOSAVdetection/macOSAVdetection/Source/DetectAuthorizationStatus.m \
+  /Users/seevogh/work/macOSAVdetection/macOSAVdetection/Source/DetectAuthorizationStatus.h \
+  //Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/include/jni.h \
+  //Library/Java/JavaVirtualMachines/jdk1.8.0_144.jdk/Contents/Home/include/darwin/jni_md.h
---
+++ macOSAVdetection/macOSAVdetection/Source/DetectAuthorizationStatus.h
@@ -0,0 +1,31 @@
+//
+//  DetectAuthorizationStatus.h
+//  macOSAVdetection
+//
+//  Created by eZuce Team on 17/04/2020.
+//  Copyright © 2020 eZuce Team. All rights reserved.
+//
+
+#include <jni.h>
+
+#ifndef DetectAuthorizationStatus_h
+#define DetectAuthorizationStatus_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_com_ezuce_utils_MacAV_askForAVPermissions
+(JNIEnv *env, jobject thiz);
+
+JNIEXPORT jbyte JNICALL Java_com_ezuce_utils_MacAV_getMicPermission
+(JNIEnv *env, jobject thiz);
+
+JNIEXPORT jbyte JNICALL Java_com_ezuce_utils_MacAV_getCamPermission
+(JNIEnv *env, jobject thiz);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* DetectAuthorizationStatus_h */
